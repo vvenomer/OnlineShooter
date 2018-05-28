8ed8b28cc2458d19ccaebe2bd4e30511955e3814
vvenomer <vvenomer@gmail.com>
2018-05-28 14:24:08 +0200

    fix

---
+++ Assets/Server/Controller.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Server
+{
+    public class Controller : MonoBehaviour
+    {
+
+        Vector2 pos;
+        float angle;
+        bool changed;
+        public void UpdatePos(Vector2 pos, float angle)
+        {
+            this.pos = pos;
+            this.angle = angle;
+            changed = true;
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            changed = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (changed)
+            {
+                transform.position = pos;
+                transform.rotation = Quaternion.Euler(0, 0, angle);
+            }
+        }
+    }
+}
